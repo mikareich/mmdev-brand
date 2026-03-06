2b1b94a8f10fca88cc23c54d211bcd45dc284fc0
Mika Reich <mikareich0@gmail.com>
2026-03-06 02:56:05 +0100

    feat(BorderBox): add asChild prop and fix guide bleed
    Use Radix Slot on the inner z-10 wrapper so consumers can
    merge BorderBox styling onto their own child element.
    
    Add bg-inherit to the outer div so the opaque background
    chains down through nested BorderBoxes, preventing sibling
    guide lines from rendering on top of content.

--- src/components/BorderBox.tsx
+++ src/components/BorderBox.tsx
@@ -1,22 +1,28 @@
+import { Slot } from "radix-ui";
 import cn from "~/utils/cn";
 
-type BorderBoxProps = React.ComponentProps<"div">;
+type BorderBoxProps = React.ComponentProps<"div"> & {
+  asChild?: boolean;
+};
 
 export default function BorderBox({
+  asChild,
   children,
   className,
   ...props
 }: BorderBoxProps) {
+  const Inner = asChild ? Slot.Root : "div";
+
   return (
     <div
-      className={cn("relative box-border p-px border-0", className)}
+      className={cn("relative box-border p-px border-0 bg-inherit", className)}
       {...props}
     >
       {/* left guide */}
-      <span className="pointer-events-none absolute left-0 top-1/2 z-0 h-[200vh] w-px -translate-y-1/2 border-[0.25px] border-inherit" />
+      <span className="pointer-events-none absolute left-0 top-1/2 z-0 h-screen w-px -translate-y-1/2 border-[0.25px] border-inherit" />
 
       {/* right guide */}
-      <span className="pointer-events-none absolute right-0 top-1/2 z-0 h-[200vh] w-px -translate-y-1/2 border-[0.25px] border-inherit" />
+      <span className="pointer-events-none absolute right-0 top-1/2 z-0 h-screen w-px -translate-y-1/2 border-[0.25px] border-inherit" />
 
       {/* top guide */}
       <span className="pointer-events-none absolute left-1/2 top-0 z-0 h-px w-[200vmax] -translate-x-1/2 border-[0.25px] border-inherit" />
@@ -24,7 +30,7 @@ export default function BorderBox({
       {/* bottom guide */}
       <span className="pointer-events-none absolute bottom-0 left-1/2 z-0 h-px w-[200vmax] -translate-x-1/2 border-[0.25px] border-inherit" />
 
-      <div className="relative z-10">{children}</div>
+      <Inner className="relative z-10">{children}</Inner>
     </div>
   );
 }
