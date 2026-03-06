bfb64aa9f81bdcc6c3c0f3ed3583588e19a4af11
Mika Reich <mikareich0@gmail.com>
2026-03-06 02:56:20 +0100

    refactor(WhoWeAre): use BorderBox asChild and content data

--- src/app/(home)/WhoWeAre.tsx
+++ src/app/(home)/WhoWeAre.tsx
@@ -1,78 +1,60 @@
 import { GitHubLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
 import Link from "next/link";
-import React from "react";
+import BorderBox from "~/components/BorderBox";
 import Section from "~/components/Section";
+import { PROFILES } from "~/content/profiles";
 
 export default function WhoWeAre() {
   return (
     <Section
       level={1}
       title="Who we are"
+      className="grid-cols-2"
       contents={[
-        <React.Fragment key={0}>
-          We are a development studio building clean, fast, and modern websites.
-          We help small businesses and startups establish their online presence
-          with zero unnecessary complexity—just results. Our focus is raw
-          performance, scalable architecture, and straightforward design.
-        </React.Fragment>,
+        <BorderBox
+          className="border-theme-border-subtle col-span-full"
+          asChild
+          key={0}
+        >
+          <p className="p-0.5 sm:p-1 bg-theme-background-accent">
+            We are a development studio building clean, fast, and modern
+            websites. We help small businesses and startups establish their
+            online presence with zero unnecessary complexity—just results. Our
+            focus is raw performance, scalable architecture, and straightforward
+            design.
+          </p>
+        </BorderBox>,
+        ...PROFILES.map(({ name, description, github, linkedin }) => (
+          <BorderBox
+            asChild
+            key={name}
+            className="hover:bg-taupe-100 border-theme-border-subtle p-1 sm:p-2"
+          >
+            <div>
+              <p className="line-clamp-4">{description}</p>
 
-        <div key={1} className="grid grid-cols-2 gap-4">
-          <div className="hover:bg-taupe-100 border-r border-theme-border-subtle">
-            <p>
-              <span className="font-bold">Marco Zillgen</span> is an experienced
-              software engineer specializing in abstract languages for the web.
-              He handles the complex engineering under the hood to ensure your
-              site is fast and flawless.
-            </p>
+              <p className="flex gap-4">
+                <Link
+                  className="flex items-center gap-1 hover:underline"
+                  href={linkedin}
+                  target="_blank"
+                >
+                  <LinkedInLogoIcon />
+                  LinkedIn
+                </Link>
 
-            <p className="flex gap-4">
-              <Link
-                className="flex items-center gap-1 hover:underline"
-                href="https://www.linkedin.com/in/marcozillgen/"
-                target="_blank"
-              >
-                <LinkedInLogoIcon />
-                LinkedIn
-              </Link>
-              <Link
-                className="flex items-center gap-1 hover:underline"
-                href="https://github.com/MarcoZillgen"
-                target="_blank"
-              >
-                <GitHubLogoIcon />
-                GitHub
-              </Link>
-            </p>
-          </div>
-
-          <div className="hover:bg-taupe-100 border-l border-theme-border-subtle">
-            <p>
-              <span className="font-bold">Mika Reich</span> is an experienced
-              software engineer specializing in abstract languages for the web.
-              He handles the complex engineering under the hood to ensure your
-              site is fast and flawless.
-            </p>
-
-            <p className="flex gap-4">
-              <Link
-                className="flex items-center gap-1 hover:underline"
-                href="https://www.linkedin.com/in/mikareich/"
-                target="_blank"
-              >
-                <LinkedInLogoIcon />
-                LinkedIn
-              </Link>
-              <Link
-                className="flex items-center gap-1 hover:underline"
-                href="https://github.com/mikareich"
-                target="_blank"
-              >
-                <GitHubLogoIcon />
-                GitHub
-              </Link>
-            </p>
-          </div>
-        </div>,
+                <Link
+                  className="flex items-center gap-1 hover:underline"
+                  href={github}
+                  target="_blank"
+                >
+                  <GitHubLogoIcon />
+                  GitHub
+                </Link>
+              </p>
+            </div>
+          </BorderBox>
+        )),
       ]}
     />
   );
