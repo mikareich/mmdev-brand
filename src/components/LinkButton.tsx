37c37cffb29ca5efd954e859d4d9eb6af3dc2403
Mika Reich <mikareich0@gmail.com>
2026-03-10 21:35:42 +0100

    chore: fmt

--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -1,22 +1,22 @@
 import Link from "next/link";
 import cn from "~/utils/cn";
-import { type ButtonProps, variants, base } from "./Button";
+import { type ButtonProps, base, variants } from "./Button";
 
 type LinkButtonProps = Omit<
-    ButtonProps,
-    keyof React.ButtonHTMLAttributes<HTMLButtonElement>
+  ButtonProps,
+  keyof React.ButtonHTMLAttributes<HTMLButtonElement>
 > &
-    React.ComponentPropsWithoutRef<typeof Link>;
+  React.ComponentPropsWithoutRef<typeof Link>;
 
 export default function LinkButton({
-    children,
-    variant,
-    className,
-    ...props
+  children,
+  variant,
+  className,
+  ...props
 }: LinkButtonProps) {
-    return (
-        <Link className={cn(base, variants[variant], className)} {...props}>
-            {children}
-        </Link>
-    );
+  return (
+    <Link className={cn(base, variants[variant], className)} {...props}>
+      {children}
+    </Link>
+  );
 }
