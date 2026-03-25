fe28cdcbef0a60871033ee67bc38f9858a2d9c65
Mika Reich <mikareich0@gmail.com>
2026-03-25 15:45:49 +0100

    fix(email): rename aws env vars

--- src/email/index.ts
+++ src/email/index.ts
@@ -1,14 +1,14 @@
 import { SESClient } from "@aws-sdk/client-ses";
 
-const { AWS_REGION, AWS_SECRET_KEY, AWS_ACCESS_KEY } = process.env;
+const { AWS_REGION, AWS_SECRET_ACCESS_KEY, AWS_ACCESS_KEY_ID } = process.env;
 
-if (!AWS_REGION || !AWS_ACCESS_KEY || !AWS_SECRET_KEY)
+if (!AWS_REGION || !AWS_ACCESS_KEY_ID || !AWS_SECRET_ACCESS_KEY)
   throw new Error("AWS Credentials not set!");
 
 export const sesClient = new SESClient({
   credentials: {
-    accessKeyId: AWS_ACCESS_KEY,
-    secretAccessKey: AWS_SECRET_KEY,
+    accessKeyId: AWS_ACCESS_KEY_ID,
+    secretAccessKey: AWS_SECRET_ACCESS_KEY,
   },
   region: AWS_REGION,
 });
