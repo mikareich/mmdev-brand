eb5f2dc5e19e9b37edd6dd5249d22cde92e7722c
Mika Reich <mikareich0@gmail.com>
2026-03-25 16:50:39 +0100

    fix(ui): simplify tw classnames

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -48,19 +48,19 @@ export default function Select({
 
       <SelectPrimitive.Portal>
         <SelectPrimitive.Content
-          className="relative z-50 max-h-96 min-w-[8rem] overflow-hidden border border-theme-border-subtle bg-theme-background text-theme-text data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1"
+          className="relative z-50 max-h-96 min-w-32 overflow-hidden border border-theme-border-subtle bg-theme-background text-theme-text data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1"
           position="popper"
         >
           <SelectPrimitive.ScrollUpButton className="flex cursor-default items-center justify-center py-1">
             <ChevronUpIcon className="h-4 w-4" />
           </SelectPrimitive.ScrollUpButton>
 
-          <SelectPrimitive.Viewport className="p-1 h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)]">
+          <SelectPrimitive.Viewport className="p-1 h-(--radix-select-trigger-height) w-full min-w-(--radix-select-trigger-width)">
             {options.map((option) => (
               <SelectPrimitive.Item
                 key={option.value}
                 value={option.value}
-                className="relative flex w-full cursor-default select-none items-center py-1.5 pl-8 pr-2 text-sm outline-none focus:bg-theme-background-accent focus:text-theme-text data-[disabled]:pointer-events-none data-[disabled]:opacity-50 transition-colors"
+                className="relative flex w-full cursor-default select-none items-center py-1.5 pl-8 pr-2 text-sm outline-none focus:bg-theme-background-accent focus:text-theme-text data-disabled:pointer-events-none data-disabled:opacity-50 transition-colors"
               >
                 <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
                   <SelectPrimitive.ItemIndicator>
